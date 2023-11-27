6d1de98e962d1f60a9440bf63a3ecf627e1a2c58
makad <16marchelo96@gmail.com>
2023-11-27 19:56:07 +0300

    fix

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -157,8 +157,8 @@ const BookingForm = () => {
 
                 <Button className={'btn'} onClick={() => {
                     const requestBody = {
-                        chatId: 170474633,
-                        // chatId: user?.id,
+                        // chatId: 170474633,
+                        chatId: user?.id,
                         clientName: clientName,
                         clientPhone: clientPhone,
                         restaurantId: restId,
